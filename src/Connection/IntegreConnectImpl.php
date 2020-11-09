0d0cdf7ff85722182814049b1609dbb157e6b097
Wagner <wagner@rigolisarosa.com>
2020-11-09 14:57:50 -0300

    Added civil status on json body.

--- src/Connection/IntegreConnectImpl.php
+++ src/Connection/IntegreConnectImpl.php
@@ -105,6 +105,7 @@ class IntegreConnectImpl implements IntegreConnect
             'Nome' => $data['name'],
             'Cpf' => $data['document'],
             'DataNascimento' => $data['birth_date'],
+            'EstadoCivil' => $data['civil_id'],
             'TelefoneFixo' => $data['telephone'],
             'TelefoneMovel' => $data['cellphone'],
             'Email' => $data['email'],
